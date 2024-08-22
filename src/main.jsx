aae9152bfc0a0bc0598525596d230ae1f654f863
rlsgabriella <rlsgabriella@gmail.com>
2024-08-22 18:44:19 -0300

    iniciando projeto react

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client';
+import Home from './pages/Home'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>,
+    <Home/>,
+  </StrictMode>,
+)
+ 
